72729884dd446b6a3a0bd0ffa1c254293cd6e672
Gaurav Patil <gauravpatil6624@gmail.com>
2022-11-04 19:52:04 +0530

    Add Files

--- UserRegistation.cs
+++ UserRegistation.cs
@@ -11,11 +11,20 @@ namespace Day19_Assingment
     {
 
         public static string REGEX_NAME = "^[A-Z]{1}[a-z]{2}[a-z]*";
-
+        //this function is use for NAME validate
         public bool validatename(string userdata)
         {
             return Regex.IsMatch(userdata, REGEX_NAME);
         }
 
+        public static string REGEX_LASTNAME = "^[A-Z]{1}[a-z]{2}[a-z]*";
+        //this function is use for NAME validate
+        public bool validatelastname(string userdata)
+        {
+            return Regex.IsMatch(userdata, REGEX_LASTNAME);
+        }
+
+
     }
 }
+
