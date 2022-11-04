29d9f0a3314f7ba033cb1d593fae5b9c18ad1bb3
Gaurav Patil <gauravpatil6624@gmail.com>
2022-11-04 20:07:14 +0530

    Add Files

--- Program.cs
+++ Program.cs
@@ -54,6 +54,22 @@
             {
                 Console.WriteLine("Please Enter Valid Mail ID");
             }
+            //program for mobile number
+            Console.WriteLine("Enter Mobile Number");
+            Console.WriteLine("NOTE:- Mobile Format - E.g. 91 9919819801 - Country code follow by space and 10 digit number");
+
+            string usermobile = Console.ReadLine();
+
+            value = userRegistration.validatemobile(usermobile);
+
+            if (value)
+            {
+                Console.WriteLine("Given Mobile Number  Accepted");
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Valid Mobile Number");
+            }
 
         }
     }
